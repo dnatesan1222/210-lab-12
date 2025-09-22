c38203b12b6421747b381c3c61e09aa6f6f21fd4
Diksha <diksha@macbookpro>
2025-09-21 20:13:43 -0700

    creating csv

--- arrayFunc.cpp
+++ arrayFunc.cpp
@@ -3,13 +3,27 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
+int main(data.txt) {
+  ...
+  ifstream fin; 
+  fin.open("data.txt");
+  if (fin.good( )) {
+    ...
+    fin.close( ); // close the file          
+    ...
+  }
+  else
+    cout << "File not found.\n";
+}
 
 
 
 int main(){
-
+    
+    string arr[5]
 
     return 0;
 }
